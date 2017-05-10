a7e8e243b1d0eb7c8b21bd9313310af60f227c02
uatilman <tefaltron1298juDE>
2017-05-10 20:46:08 +0300

    get запрос

--- app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
+++ app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
@@ -8,11 +8,19 @@ import android.widget.LinearLayout;
 
 import com.example.lesson6.R;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class TopActivity extends AppCompatActivity implements View.OnClickListener {
 
     private LinearLayout hearLay;
     private LinearLayout nailLay;
     private LinearLayout addressLay;
+
+    HttpURLConnection urlConnection;
 
 
     @Override
@@ -43,12 +51,40 @@ public class TopActivity extends AppCompatActivity implements View.OnClickListen
         if (view.equals(nailLay)) {
             intent = new Intent(this, CategoryActivity.class);
             intent.putExtra(Services.TYPE, Services.Services_TYPE.NAIL);
+            startActivity(intent);
         } else if (view.equals(hearLay)) {
             intent = new Intent(this, CategoryActivity.class);
             intent.putExtra(Services.TYPE, Services.Services_TYPE.HAIR);
+            startActivity(intent);
         } else {
-            intent = new Intent(this, AddressActivity.class);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        URL url = new URL("http://192.168.1.40/juD/");
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        BufferedInputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        int c;
+                        in.read();
+                        System.out.println((byte)in.read());
+                       while ((c = in.read()) != -1) {
+
+                            System.out.print((char) c);
+                        }
+
+                        urlConnection.disconnect();
+                    } catch (IOException e) {
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+
+                }
+            }).start();
+
+
+            //intent = new Intent(this, AddressActivity.class);
         }
-        startActivity(intent);
+        //startActivity(intent);
     }
 }
