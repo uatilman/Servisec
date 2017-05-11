5db92d63b1f45afc7a405fbc3e81cc7bbfbb01d9
uatilman <uatilman@gmail.com>
2017-05-11 08:39:29 +0300

    добавлоен комментарий

--- app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
+++ app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
@@ -71,6 +71,7 @@ public class TopActivity extends AppCompatActivity implements View.OnClickListen
                        while ((c = in.read()) != -1) {
                             System.out.print((char) c);
                         }
+//комментарий для тестирования git
 
                         urlConnection.disconnect();
                     } catch (IOException e) {
