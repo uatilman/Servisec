0771a8cc670ec86413e54576fe49171cc9a86316
uatilman <uatilman@gmail.com>
2017-05-11 08:40:15 +0300

    тест тест

--- app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
+++ app/src/main/java/com/example/yuriytilman/servisec/TopActivity.java
@@ -83,6 +83,7 @@ public class TopActivity extends AppCompatActivity implements View.OnClickListen
 
 
             //intent = new Intent(this, AddressActivity.class);
+            //ткст тест intent = new Intent(this, AddressActivity.class);
         }
         //startActivity(intent);
     }
